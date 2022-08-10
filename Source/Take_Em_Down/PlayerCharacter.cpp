3e10c3880d6f524cf0d9e9e6b78fc0f93f084588
Deepak Giri <deepakgiri735055@gmail.com>
2022-08-10 22:44:42 +0530

    CPP

--- Source/Take_Em_Down/PlayerCharacter.cpp
+++ Source/Take_Em_Down/PlayerCharacter.cpp
@@ -2,14 +2,94 @@
 
 
 #include "PlayerCharacter.h"
-
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 // Sets default values
-APlayerCharacter::APlayerCharacter()
+APlayerCharacter::APlayerCharacter():BaseTurnRate(50.f), BaseLookUpRate(50.f), bAiming(false), HipLookUpRate(90.f), HipTurnRate(90.f), AimLookUpRate(20.f), AimTurnRate(20.f),
+MouseHipTurnRate(1.0f), MouseHipLookUpRate(1.0f), MouseAimTurnRate(0.2f), MouseAimLookUpRate(0.2f)
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	GetCharacterMovement()->JumpZVelocity = 690.f;
+	GetCharacterMovement()->MaxWalkSpeed = 450.f;
+	GetCharacterMovement()->AirControl = 0.3f;
+	GetCharacterMovement()->bOrientRotationToMovement = false;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 540.f, 0.f);
+
 }
+
+void APlayerCharacter::MoveForward(float value)
+{
+	if ((Controller != nullptr) && (value != 0))
+	{
+		const FRotator ControllerRotation = Controller->GetControlRotation();
+		const FRotator Controller_Yaw = FRotator(0.f, ControllerRotation.Yaw, 0.f);
+		const FVector Direction = FRotationMatrix{ Controller_Yaw }.GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, value);
+	}
+}
+
+void APlayerCharacter::MoveRight(float value)
+{
+	if ((Controller != nullptr) && (value != 0))
+	{
+		const FRotator ControllerRotation = Controller->GetControlRotation();
+		const FRotator Controller_Yaw = FRotator(0.f, ControllerRotation.Yaw, 0.f);
+		const FVector Direction = FRotationMatrix{ Controller_Yaw }.GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, value);
+	}
+}
+
+void APlayerCharacter::TurnAtRate(float Rate)
+{
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void APlayerCharacter::LookUpRate(float Rate)
+{
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void APlayerCharacter::Turn(float value)
+{
+	float TurnScaleFactor{};
+	if (bAiming)
+	{
+		TurnScaleFactor = MouseAimTurnRate;
+	}
+	else
+	{
+		TurnScaleFactor = MouseHipTurnRate;
+	} AddControllerYawInput(value * TurnScaleFactor);
+}
+
+void APlayerCharacter::LookUp(float value)
+{
+	float LookUpScaleFactor{};
+	if (bAiming)
+	{
+		LookUpScaleFactor = MouseAimLookUpRate;
+	}
+	else
+	{
+		LookUpScaleFactor = MouseHipLookUpRate;
+	} AddControllerPitchInput(value * LookUpScaleFactor);
+}
+
+void APlayerCharacter::AimingButtonPressed()
+{
+	bAiming = true;
+}
+
+void APlayerCharacter::AimingButtonReleased()
+{
+	bAiming = false;
+}
+
 
 // Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
@@ -29,5 +109,13 @@ void APlayerCharacter::Tick(float DeltaTime)
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	check(PlayerInputComponent)
+		PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &APlayerCharacter::LookUpRate);
+	PlayerInputComponent->BindAxis("TurnRate", this, &APlayerCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::LookUp);
+	PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::Turn);
+	PlayerInputComponent->BindAction("Aim", IE_Pressed, this, &APlayerCharacter::AimingButtonPressed);
+	PlayerInputComponent->BindAction("Aim", IE_Released, this, &APlayerCharacter::AimingButtonReleased);
 }
