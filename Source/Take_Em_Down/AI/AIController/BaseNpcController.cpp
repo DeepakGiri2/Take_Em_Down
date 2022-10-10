03d506350f558c87b1496cb0f7aa9df0b800ae40
Deepak Giri <deepakgiri735055@gmail.com>
2022-10-11 00:38:26 +0530

    Behavior Tree

--- Source/Take_Em_Down/AI/AIController/BaseNpcController.cpp
+++ Source/Take_Em_Down/AI/AIController/BaseNpcController.cpp
@@ -8,24 +8,24 @@
 #include "Take_Em_Down/Character/PlayerCharacter.h"
 #include "BehaviorTree/BehaviorTree.h"
 #include "BehaviorTree/BlackboardComponent.h"
-ABaseNpcController::ABaseNpcController():AISightRadius(500.f), AISightAge(5.0f),
-AILooseSightRadius(AISightRadius + 50.f), AIFOV(145.f)
+ABaseNpcController::ABaseNpcController(FObjectInitializer const& object_intializer):m_AISightRadius(500.f), m_AISightAge(5.0f),
+m_AILooseSightRadius(m_AISightRadius + 50.f), m_AIFOV(145.f)
 {
 	PrimaryActorTick.bCanEverTick = true;
-    SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>("NoSense");
+	m_SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>("NoSense");
 	SetPerceptionComponent(*CreateDefaultSubobject<UAIPerceptionComponent>("Per Comp"));
-	SightConfig->SightRadius = AISightRadius;
-	SightConfig->LoseSightRadius = AILooseSightRadius;
-	SightConfig->PeripheralVisionAngleDegrees = AIFOV;
-	SightConfig->SetMaxAge(AISightAge);
-	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
-	SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
-	SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+	m_SightConfig->SightRadius = m_AISightRadius;
+	m_SightConfig->LoseSightRadius = m_AILooseSightRadius;
+	m_SightConfig->PeripheralVisionAngleDegrees = m_AIFOV;
+	m_SightConfig->SetMaxAge(m_AISightAge);
+	m_SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+	m_SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+	m_SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
 
-	GetPerceptionComponent()->SetDominantSense(*SightConfig->GetSenseImplementation());
+	GetPerceptionComponent()->SetDominantSense(*m_SightConfig->GetSenseImplementation());
 	GetPerceptionComponent()->OnPerceptionUpdated.AddDynamic(this, &ThisClass::OnPawnDetected);
 	GetPerceptionComponent()->OnTargetPerceptionUpdated.AddDynamic(this, &ThisClass::OnTargetUpdate);
-	GetPerceptionComponent()->ConfigureSense(*SightConfig);
+	GetPerceptionComponent()->ConfigureSense(*m_SightConfig);
 }
 
 void ABaseNpcController::BeginPlay()
@@ -42,21 +42,21 @@ void ABaseNpcController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
 	TArray<AActor*> OutActor;
-	UGameplayStatics::GetAllActorsOfClass(this, GitClass, OutActor);
+	UGameplayStatics::GetAllActorsOfClass(this, m_GitClass, OutActor);
 	for (AActor* A : OutActor)
 	{
-		ACT = Cast<APlayerCharacter>(A);
-		if (ACT && ACT->IsLocallyControlled())
+		m_ACT = Cast<APlayerCharacter>(A);
+		if (m_ACT && m_ACT->IsLocallyControlled())
 		{
 			return;
 		}
 	}
-	
+	RunBehaviorTree(m_BT_Asset);
 }
 
 FRotator ABaseNpcController::GetControlRotation() const
 {
-	if (GetPawn() == nullptr)
+	if (!GetPawn())
 	{
 		return FRotator(0.f,0.f,0.f);
 	}
@@ -69,7 +69,7 @@ void ABaseNpcController::OnPawnDetected(const TArray<AActor*>& UpdatedActors)
 
 void ABaseNpcController::OnTargetUpdate(AActor* Actor, FAIStimulus Stimulus)
 {
-	if (Stimulus.WasSuccessfullySensed() && Actor == ACT)
+	if (Stimulus.WasSuccessfullySensed() && Actor == m_ACT)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Hello my name is "));
 	}
--- Source/Take_Em_Down/AI/AIController/BaseNpcController.h
+++ Source/Take_Em_Down/AI/AIController/BaseNpcController.h
@@ -19,29 +19,36 @@ class TAKE_EM_DOWN_API ABaseNpcController : public AAIController , public IAIInt
 protected:
 	virtual void BeginPlay() override;
 public:
-	ABaseNpcController();
+	ABaseNpcController(FObjectInitializer const& object_intializer = FObjectInitializer::Get());
 	
 	virtual void OnPossess(APawn* InPawn) override;
 	virtual void Tick(float DeltaTime) override;
 	virtual FRotator GetControlRotation() const override;
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Comps, meta = (AllowPrivateAccess = "true"))
+	class UBehaviorTreeComponent* m_BTComp;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Comps, meta = (AllowPrivateAccess = "true"))
+	class UBehaviorTree* m_BT_Asset;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Comps, meta = (AllowPrivateAccess = "true"))
+		class UBlackboardComponent* m_BB_Comp;
 public:
 	UFUNCTION()
 		void OnPawnDetected(const TArray<AActor*>& UpdatedActors);
 	UFUNCTION()
 		void OnTargetUpdate(AActor* Actor, FAIStimulus Stimulus);
 	UPROPERTY(BlueprintReadWrite, Category = Properites, meta = (AllowPrivateAccess = "true"))
-		float AISightRadius;
+		float m_AISightRadius;
 	UPROPERTY(BlueprintReadWrite, Category = Properites, meta = (AllowPrivateAccess = "true"))
-		float AISightAge;
+		float m_AISightAge;
 	UPROPERTY(BlueprintReadWrite, Category = Properites, meta = (AllowPrivateAccess = "true"))
-		float AILooseSightRadius;
+		float m_AILooseSightRadius;
 	UPROPERTY(BlueprintReadWrite, Category = Properites, meta = (AllowPrivateAccess = "true"))
-		float AIFOV;
+		float m_AIFOV;
 	UPROPERTY(BlueprintReadWrite, Category = AI, meta = (AllowPrivateAccess = "true"))
-		TObjectPtr<class UAISenseConfig_Sight> SightConfig;
+		TObjectPtr<class UAISenseConfig_Sight> m_SightConfig;
 	UPROPERTY(BlueprintReadWrite, Category = Variables, meta = (AllowPrivateAccess = "true"))
-		TObjectPtr<class APlayerCharacter> ACT;
+		TObjectPtr<class APlayerCharacter> m_ACT;
 	UPROPERTY(BlueprintReadWrite, Category = Variables, meta = (AllowPrivateAccess = "true"))
-		TSubclassOf<AActor> GitClass;
+		TSubclassOf<AActor> m_GitClass;
 
 };
